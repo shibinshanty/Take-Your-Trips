6c130635c17218d9a73406a226572f1168ee6ea6
shibinshanty <shibinshanty612000@gmail.com>
2025-06-12 14:12:32 +0530

    changed in server.js

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,9 @@ const bookingRoute = require('./routes/bookingRoute');
 const contactRoute=require('./routes/contactRoute')
 
 
+//middlware
+app.use(cors())
+app.use(express.json())
 
 //DB connection
 mongoose.connect(process.env.DB_URL).then(()=>{console.log("DB is connected sucessfully")}
@@ -35,9 +38,7 @@ app.use('/api',contactRoute);
 
 
 
-//middlware
-app.use(cors())
-app.use(express.json())
+
 
 const PORT=process.env.PORT || 5000
 app.listen(PORT,()=>{console.log(`example app listening on port ${PORT}`)})
