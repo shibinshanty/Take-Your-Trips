499ca3bbee8d6d9909d91ff810e5168e8274efe9
shibinshanty <shibinshanty612000@gmail.com>
2025-06-26 13:13:31 +0530

    edited shimmer ui component

--- frontend/src/pages/Shimmer.jsx
+++ frontend/src/pages/Shimmer.jsx
@@ -1,12 +1,10 @@
-
-
-const ShimmerUi = () => {
+const Shimmer = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-10">
       {Array(10).fill("").map((_, index) => (
         <div
           key={index}
-          className="w-64 h-64 bg-gray-200 rounded-2xl animate-pulse shadow-md"
+          className="w-full h-64 bg-gray-200 rounded-2xl animate-pulse shadow-md"
         >
           <div className="h-44 bg-gray-300 rounded-t-2xl"></div>
           <div className="p-4 space-y-2">
@@ -20,4 +18,4 @@ const ShimmerUi = () => {
   );
 };
 
-export default ShimmerUi;
+export default Shimmer;
